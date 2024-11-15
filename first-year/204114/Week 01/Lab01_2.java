4dd6d18f6983850f7f966f38e37ffb5adc07ecc2
nacs-970 <atitape6958@gmail.com>
2024-11-15 13:16:07 +0700

    11/15/24-01:16:07 PM

--- first-year/204114/Week 01/Lab01_2.java
+++ first-year/204114/Week 01/Lab01_2.java
@@ -6,26 +6,33 @@ public class Lab01_2 {
 
     Scanner kb = new Scanner(System.in);
     int in = kb.nextInt();
-    double sum = in, count = 0, remain = 0, change = 0;
+    int sum = in, count = 0, remain = 0, change = 0;
 
     if (in >= 5){
+      // find exceed bottle
       remain = in%5;
       in -= remain;
-      change = ((float)in/5)*2;
+
+      change = (in/5)*2;
       sum += change;
+
+      // add exceed back for next calculation
       change += remain;
       count++;
     }
 
     while (change >= 5) {
-      count++;
+
       remain = change%5;
       change -= remain;
+
       change = (change/5)*2;
       sum += change;
+
       change += remain;
+      count++;
     }
 
-    System.out.printf("%d %d %d\n", (int)Math.rint(sum), (int)count, (int)Math.rint(sum)%5);
+    System.out.printf("%d %d %d\n", sum, count, (sum%5));
   }
 }
